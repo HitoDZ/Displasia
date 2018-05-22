8fe1f5aa4b34613099b56ce11e194be202fb1b27
HitoDZ <hitodz97@gmail.com>
2018-05-22 20:05:51 +0300

    UploadImage
    add upload image
    change some index.html

--- server/server/Controllers/HomeController.cs
+++ server/server/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using server.Models;
 
@@ -32,6 +33,59 @@ namespace server.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        [HttpPost]
+        public String Rest(IFormFile fileToUpload)
+        {
+            /*WebRequest request = WebRequest.Create("http://127.0.0.1:5002/test11");
+            request.Method = "POST";
+            string postData = "";
+                if (fileToUpload.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                       fileToUpload.CopyTo(ms);
+                        var fileBytes = ms.ToArray();
+                        postData  = Convert.ToBase64String(fileBytes);
+                        // act on the Base64 data
+                    }
+                }
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.  
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+
+
+
+            WebResponse response = request.GetResponse();
+
+
+
+            // Display the status.  
+           // Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+           // Console.WriteLine(responseFromServer);
+            // Clean up the streams.  
+            reader.Close();
+            dataStream.Close();
+            response.Close();* */
+    
+            return "Oh Yeah2";   
         }
     }
 }
--- server/server/wwwroot/js/site.js
+++ server/server/wwwroot/js/site.js
@@ -1,1 +1,22 @@
 ﻿// Write your JavaScript code.
+function uploadFile() {
+    //  var blobFile = $('#filechooser').files[0];
+    var blobFile = document.getElementById("filechooser").files[0];
+    var formData = new FormData();
+    formData.append("fileToUpload", blobFile);
+    //debugger;
+
+    $.ajax({
+        url: "http://localhost:5000/Home/Rest",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+            console.log(response); // .. do something
+        },
+        error: function(jqXHR, textStatus, errorMessage) {
+            console.log(errorMessage); // Optional
+        }
+    });
+}
